4c9877a1815f8195de3be9a38342c61e231c7b88
Joerdan Newue <joerdannewue@gmail.com>
2020-07-13 10:35:20 +0300

    оформил шифр вижинера как полагается

--- Vision_code.py
+++ Vision_code.py
@@ -1,11 +1,10 @@
 bukvar = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-numberal = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32]
 message = str(input("введите сообщение для шифрования"))
 key = str(input("введите ключ для кодирования, слово из русских букв"))
 key = key.lower()
 key *= len(message) // len(key) + 1
 print(key)
+schet=0
 ckount = []
 endcount = []
 strangeword = ""
@@ -17,10 +16,8 @@ for litera in ckount:
     endcount.append(secret_pose)
 for bukva in message:
     position = bukvar.find(bukva)
-    p=0
-    codeposition = position + endcount[p]
-    for o in range(len(key)):
-        p=p+1
+    codeposition = position + endcount[schet]
+    schet=schet+1
     if bukva in bukvar:
         strangeword = strangeword + bukvar[codeposition]
     else:
--- proba_ne_otnositsya_k_uroku.py
+++ proba_ne_otnositsya_k_uroku.py
@@ -1,6 +1,7 @@
-bukvar = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-endcount=[]
-for litera in bukvar:
-    secret_pose = bukvar.find(litera)
-    endcount.append(secret_pose)
-print(endcount)
+code = 0
+i= 0
+endcount = [1, 2, 3, 4]
+for number in endcount:
+    i=i+1
+    code = code + endcount[i]
+    print(code)
