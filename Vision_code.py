b493f63f6339b43b63501e23ac6861f449342842
Joerdan Newue <joerdannewue@gmail.com>
2020-07-13 09:07:37 +0300

    сделал, но следует отработать строку 21(счётчик не работает)

--- Vision_code.py
+++ Vision_code.py
@@ -1,14 +1,28 @@
 bukvar = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-numberal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32, 33]
+numberal = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32]
 message = str(input("введите сообщение для шифрования"))
 key = str(input("введите ключ для кодирования, слово из русских букв"))
 key = key.lower()
-ckount= []
+key *= len(message) // len(key) + 1
+print(key)
+ckount = []
+endcount = []
 strangeword = ""
 for i, j in enumerate(key):
     print(i, j)
-    for g in bukvar:
-        pos_keya = bukvar.find(g)
-        if j==g:
-            for 
+    ckount.append(j)
+for litera in ckount:
+    secret_pose = bukvar.find(litera)
+    endcount.append(secret_pose)
+for bukva in message:
+    position = bukvar.find(bukva)
+    p=0
+    codeposition = position + endcount[p]
+    for o in range(len(key)):
+        p=p+1
+    if bukva in bukvar:
+        strangeword = strangeword + bukvar[codeposition]
+    else:
+        print("вы ввели недопустимый символ")
+print("теперь ваш текст не прочитать без ключа! выглядит он так:", strangeword)
--- proba_ne_otnositsya_k_uroku.py
+++ proba_ne_otnositsya_k_uroku.py
@@ -1,12 +1,6 @@
-a = [0] * 4
-for i in range(len(a)):
-    i = str(i + 1)
-    print("Введите элемент массива " + i, end = " ")
-    i = int(i)
-    i = i - 1
-    a[i] = int(input())
-print("")
-for i in range(len(a)):
-    a[i] = a[i] * 2
-for i in range(len(a)):
-    print(a[i], end = " ")
+bukvar = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+endcount=[]
+for litera in bukvar:
+    secret_pose = bukvar.find(litera)
+    endcount.append(secret_pose)
+print(endcount)
